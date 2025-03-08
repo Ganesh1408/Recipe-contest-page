020b9d179ccec14d3276f020f5f310b3b5e3ed33
Ganesh1408 <somariganesh@gmail.com>
2025-03-08 12:53:26 +0530

    remove padding-left at recipe card

--- src/Components/RecipeCard/index.jsx
+++ src/Components/RecipeCard/index.jsx
@@ -1,43 +1,35 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 import "./styles.css";
 
-
-
 function RecipeCard({ recipe }) {
-    
-
-    if(!recipe){
-        return <p>No recipes found</p>
-    }
+  if (!recipe) {
+    return <p>No recipes found</p>;
+  }
   //returing jsx
   return (
     <>
-    
-          <li className="RecipeCard">
-            
-            <LazyLoadImage
-              src={recipe.imgUrl}
-              alt={recipe.name}
-              effect="blur"
-              className="RecipeCard_image"
-            />
-            <h2>{recipe.name}</h2>
-            <p>{recipe.description}</p>
-            <p>Chef : {recipe.chef}</p>
-            <p>MealType : {recipe.mealType}</p>
-            <p>DishType : {recipe.dishType}</p>
-            <p>Uploaded On : {recipe.uploadedOn}</p>
-            <p>Ratings : {recipe.avgRating} | {recipe.totalRatings}</p>
-            <p>KitchTest : {recipe.testKitchenApproved.toString()}</p>
-            <p>Contest Winner : {recipe.contestWinner.toString()}</p>
-            <p>Featured : {recipe.featured.toString()}</p>
-            
+      <li className="RecipeCard">
+        <LazyLoadImage
+          src={recipe.imgUrl}
+          alt={recipe.name}
+          className="RecipeCard_image"
+        />
 
-          </li>
-       
+        <h2>{recipe.name}</h2>
+        <p>{recipe.description}</p>
+        <p>Chef : {recipe.chef}</p>
+        <p>MealType : {recipe.mealType}</p>
+        <p>DishType : {recipe.dishType}</p>
+        <p>Uploaded On : {recipe.uploadedOn}</p>
+        <p>
+          Ratings : {recipe.avgRating} | {recipe.totalRatings}
+        </p>
+        <p>KitchTest : {recipe.testKitchenApproved.toString()}</p>
+        <p>Contest Winner : {recipe.contestWinner.toString()}</p>
+        <p>Featured : {recipe.featured.toString()}</p>
+      </li>
     </>
   );
 }
